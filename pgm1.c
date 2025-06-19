ef4dd678088e3bf1dbd47004e3bc89c320137ad9
GlauberH <glauber.sena@a.ucb.br>
2025-06-18 21:01:24 -0300

    Update pgm1.c

--- pgm1.c
+++ pgm1.c
@@ -33,6 +33,7 @@ typedef struct {
 Leitura dados[MAX_LEIT];
 InfoSensor sensores[MAX_SENS];
 int numLeit = 0, numSens = 0;
+int linhaAtual = 0;
 
 TipoDado detectarTipo(char* str) {
     if (!strcmp(str, "true") || !strcmp(str, "false")) 
@@ -48,24 +49,27 @@ TipoDado detectarTipo(char* str) {
     return TIPO_STR;
 }
 
-Valor converterValor(char* str, TipoDado t) {
+Valor convValor(char* str, TipoDado t) {
     Valor v;
     switch(t) {
         case TIPO_INT: v.inteiro = atoi(str); break;
         case TIPO_BOOL: v.booleano = !strcmp(str, "true"); break;
         case TIPO_FLOAT: v.racional = atof(str); break;
-        case TIPO_STR: strcpy(v.string, str); break;
+        case TIPO_STR: 
+            strncpy(v.string, str, sizeof(v.string) - 1);
+            v.string[sizeof(v.string) - 1] = '\0';
+            break;
     }
     return v;
 }
 
-// Ordenação
+// Ordenação Decrescente
 
-void ordenarDados(Leitura *arr, int n) {
+void ordenarDadosDescres(Leitura *arr, int n) {
     for(int i = 0; i < n-1; i++) {
         bool trocou = false;
         for(int j = 0; j < n-i-1; j++) {
-            if(arr[j].timestamp > arr[j+1].timestamp) {
+            if(arr[j].timestamp < arr[j+1].timestamp) {
                 Leitura tmp = arr[j];
                 arr[j] = arr[j+1]; 
                 arr[j+1] = tmp;
@@ -85,14 +89,55 @@ void escreverValor(FILE *f, Valor v, TipoDado t) {
     }
 }
 
+bool valLinha(const char* linha, char* timestampStr, char* sensorTemp, char* valorStr) {
+    char linhaCopia[256];
+    strncpy(linhaCopia, linha, sizeof(linhaCopia) - 1);
+    linhaCopia[sizeof(linhaCopia) - 1] = '\0';
+    
+    int len = strlen(linhaCopia);
+    if (len > 0 && linhaCopia[len-1] == '\n') {
+        linhaCopia[len-1] = '\0';
+    }
+    
+    if (strlen(linhaCopia) == 0) {
+        return false;
+    }
+    
+    int campos = sscanf(linhaCopia, "%s %s %s", timestampStr, sensorTemp, valorStr);
+    
+    if (campos != 3) {
+        return false;
+    }
+    
+    char *end;
+    long ts = strtol(timestampStr, &end, 10);
+    if (*end != '\0' || ts <= 0) {
+        return false;
+    }
+    
+    if (strlen(sensorTemp) == 0 || strlen(sensorTemp) >= 20) {
+        return false;
+    }
+    
+    if (strlen(valorStr) == 0) {
+        return false;
+    }
+    
+    return true;
+}
+
 int main(int argc, char **argv) {
     char nomeArq[256];
     
     if(argc == 2) {
         strcpy(nomeArq, argv[1]);
     } else {
+        printf("Uso: %s <nome_arquivo>\n", argv[0]);
         printf("Digite o nome do arquivo: ");
-        scanf("%s", nomeArq);
+        if (scanf("%255s", nomeArq) != 1) {
+            printf("Erro ao ler nome do arquivo\n");
+            return 1;
+        }
     }
     
     FILE *f = fopen(nomeArq, "r");
@@ -102,18 +147,26 @@ int main(int argc, char **argv) {
     }
 
     char linha[256], timestampStr[32], sensorTemp[20], valorStr[20];
+    linhaAtual = 0;
+    int linhasInvalidas = 0;
     
     while(fgets(linha, sizeof(linha), f)) {
-        if(sscanf(linha, "%s %s %s", timestampStr, sensorTemp, valorStr) != 3) 
+        linhaAtual++;
+        
+        if (!valLinha(linha, timestampStr, sensorTemp, valorStr)) {
+            printf("AVISO: Linha %d inválida: %s", linhaAtual, linha);
+            linhasInvalidas++;
             continue;
-            
+        }
+        
         if(numLeit >= MAX_LEIT) {
             printf("Limite de leituras atingido: %d\n", MAX_LEIT);
             break;
         }
         
         dados[numLeit].timestamp = atol(timestampStr);
-        strcpy(dados[numLeit].sensor, sensorTemp);
+        strncpy(dados[numLeit].sensor, sensorTemp, sizeof(dados[numLeit].sensor) - 1);
+        dados[numLeit].sensor[sizeof(dados[numLeit].sensor) - 1] = '\0';
         
         TipoDado tipoDetect = detectarTipo(valorStr);
         
@@ -129,23 +182,28 @@ int main(int argc, char **argv) {
         }
         
         // Novo sensor
-        
+
         if(indSensor == -1) {
-            if(numSens >= MAX_SENS) break;
-            strcpy(sensores[numSens].nome, sensorTemp);
+            if(numSens >= MAX_SENS) {
+                printf("Limite de sensores atingido: %d\n", MAX_SENS);
+                break;
+            }
+            strncpy(sensores[numSens].nome, sensorTemp, sizeof(sensores[numSens].nome) - 1);
+            sensores[numSens].nome[sizeof(sensores[numSens].nome) - 1] = '\0';
             sensores[numSens].tipo = tipoDetect;
             sensores[numSens].count = 0;
             indSensor = numSens++;
         }
         
         dados[numLeit].tipo = tipoDetect;
-        dados[numLeit].valor = converterValor(valorStr, tipoDetect);
+        dados[numLeit].valor = convValor(valorStr, tipoDetect);
         sensores[indSensor].count++;
         numLeit++;
     }
     fclose(f);
     
-    printf("Total de leituras: %d\n", numLeit);
+    printf("Total de leituras válidas: %d\n", numLeit);
+    printf("Total de linhas inválidas: %d\n", linhasInvalidas);
     printf("Sensores encontrados: %d\n", numSens);
     
     for(int i = 0; i < numSens; i++) {
@@ -154,13 +212,18 @@ int main(int argc, char **argv) {
                nomesTipo[sensores[i].tipo]);
     }
     
-    // Arquivos por sensor
+    // Arquivos por sensor (Ordem Decrecente)
 
     for(int s = 0; s < numSens; s++) {
         char nomeArqSens[30];
-        sprintf(nomeArqSens, "%s.txt", sensores[s].nome);
+        snprintf(nomeArqSens, sizeof(nomeArqSens), "%s.txt", sensores[s].nome);
         
         Leitura *dadosSensor = malloc(sensores[s].count * sizeof(Leitura));
+        if (!dadosSensor) {
+            printf("Erro de alocação de memória para sensor %s\n", sensores[s].nome);
+            continue;
+        }
+        
         int countSensor = 0;
         
         for(int i = 0; i < numLeit; i++) {
@@ -168,8 +231,8 @@ int main(int argc, char **argv) {
                 dadosSensor[countSensor++] = dados[i];
             }
         }
-        
-        ordenarDados(dadosSensor, countSensor);
+
+        ordenarDadosDescres(dadosSensor, countSensor);
         
         FILE *arq = fopen(nomeArqSens, "w");
         if(arq) {
@@ -179,7 +242,9 @@ int main(int argc, char **argv) {
                 fprintf(arq, "\n");
             }
             fclose(arq);
-            printf("Arquivo %s criado com %d leituras.\n", nomeArqSens, countSensor);
+            printf("Arquivo %s criado com %d leituras (ordem DECRESCENTE).\n", nomeArqSens, countSensor);
+        } else {
+            printf("Erro ao criar arquivo %s\n", nomeArqSens);
         }
         free(dadosSensor);
     }
